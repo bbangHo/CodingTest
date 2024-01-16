79f910aed1e8c6573b64024f0e78c7d119ded249
오승훈 <120100354+os-hoon@users.noreply.github.com>
2024-01-17 03:00:49 +0900

    update

--- construct/MemberThis.java
+++
@@ -1,8 +0,0 @@
-package construct;
-
-public class MemberThis {
-    String nameField;
-    void initMember(String nameParameter) {
-        this.nameField = nameParameter; // 요즘은 멤버변수 색깔로 잘 구분해주기 때문에 지역(매개)변수랑 이름 다르면 굳이 쓸 필요없음!
-    }
-}
