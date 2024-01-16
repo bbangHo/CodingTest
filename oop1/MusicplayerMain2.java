c0e9cf5cb7c20afaf65b6cf209ccb9b7507baadd
오승훈 <120100354+os-hoon@users.noreply.github.com>
2024-01-17 03:02:34 +0900

    섹션 3. 소스코드

---
+++ oop1/MusicplayerMain2.java
@@ -0,0 +1,31 @@
+package oop1;
+
+public class MusicplayerMain2 {
+    public static void main(String[] args) {
+        MusicplayerData data = new MusicplayerData();
+
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다");
+
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+
+        System.out.println("음악 플레이어 상태 확인");
+        if(data.isOn){
+            System.out.println("음악 플레이어 ON, 볼륨:" + data.volume);
+        }
+        else{
+            System.out.println("음악 플레이어 OFF");
+        }
+
+        data.isOn = false;
+        System.out.println("음악 플레이어를 종료합니다");
+
+    }
+}
